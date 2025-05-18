eb6c067a189a78625b84c16f52ef34b1a0d1ed67
uno-sw <uno-sw@live.jp>
2025-05-18 22:30:57 +0900

    Implement audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:simple_synth/web_audio.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +15,45 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  final _audioContext = AudioContext();
+
+  bool _isInitialized = false;
   bool _isPlaying = false;
+
+  Future<void> _setUpAudio() async {
+    _audioContext.createOscillator()
+      ..type = 'sine'
+      ..frequency.setValueAtTime(440, _audioContext.currentTime)
+      ..connect(_audioContext.destination)
+      ..start();
+    await _audioContext.suspend().toDart;
+    setState(() {
+      _isInitialized = true;
+    });
+  }
+
+  Future<void> _toggleAudio() async {
+    switch (_audioContext.state) {
+      case 'suspended':
+        await _audioContext.resume().toDart;
+      case 'running':
+        await _audioContext.suspend().toDart;
+      default:
+        throw StateError(
+          'AudioContext is in unknown state: ${_audioContext.state}',
+        );
+    }
+
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioContext.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +66,11 @@ class _MainAppState extends State<MainApp> {
             style: FilledButton.styleFrom(
               backgroundColor: _isPlaying ? colors.error : colors.primary,
             ),
-            onPressed: () {
-              setState(() {
-                _isPlaying = !_isPlaying;
-              });
+            onPressed: () async {
+              if (!_isInitialized) {
+                await _setUpAudio();
+              }
+              await _toggleAudio();
             },
             child: Text(_isPlaying ? 'Stop' : 'Play'),
           ),
