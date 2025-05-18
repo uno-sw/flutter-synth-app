d59cd00f912fbf8671b8b81eff3a07de1ee72ada
uno-sw <uno-sw@live.jp>
2025-05-18 22:29:39 +0900

    Implement play/stop UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,34 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool _isPlaying = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final colors = Theme.of(context).colorScheme;
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: _isPlaying ? colors.error : colors.primary,
+            ),
+            onPressed: () {
+              setState(() {
+                _isPlaying = !_isPlaying;
+              });
+            },
+            child: Text(_isPlaying ? 'Stop' : 'Play'),
+          ),
         ),
       ),
     );
